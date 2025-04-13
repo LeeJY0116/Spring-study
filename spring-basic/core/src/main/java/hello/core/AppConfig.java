d125dc7ab57b9ad7a5231aa73450dc59fa22a692
Juyeob Lee <lcg0116@gmail.com>
2025-04-14 01:25:19 +0900

    2025-04-13 spring-basic

--- spring-basic/core/src/main/java/hello/core/AppConfig.java
+++ spring-basic/core/src/main/java/hello/core/AppConfig.java
@@ -1,14 +1,13 @@
 package hello.core;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
-import hello.core.member.MemberServiceimpl;
+import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
-import hello.core.order.OrderServiceimpl;
+import hello.core.order.OrderServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -18,7 +17,7 @@ public class AppConfig {
 
     @Bean
     public MemberService memberService() {
-        return new MemberServiceimpl(memberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
@@ -28,7 +27,7 @@ public class AppConfig {
 
     @Bean
     public OrderService orderService() {
-        return new OrderServiceimpl(new MemoryMemberRepository(), discountPolicy());
+        return new OrderServiceImpl(new MemoryMemberRepository(), discountPolicy());
     }
 
     @Bean
--- spring-basic/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ spring-basic/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,14 +3,13 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
-import hello.core.member.MemoryMemberRepository;
 
-public class OrderServiceimpl implements OrderService {
+public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
-    public OrderServiceimpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
